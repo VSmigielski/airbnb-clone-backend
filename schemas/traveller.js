9418a84bedd9fbca9935037280e5e5ead34d8fec
Veronica <veronicasmigielski@gmail.com>
2021-09-05 23:43:39 +0200

    Finished backend

---
+++ schemas/traveller.js
@@ -0,0 +1,20 @@
+export default {
+    name: 'traveller',
+    title: 'Traveller',
+    type: 'reference',
+    to: [{ type: 'person' }],
+    preview: {
+      select: {
+        name: 'person.name',
+        media: 'person.image',
+      },
+      prepare(selection) {
+        const { name, media } = selection
+        return {
+          title: name,
+          media,
+        }
+      },
+    },
+  }
+  
